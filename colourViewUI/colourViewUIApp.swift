58e6c111e91b789fc71cb9137a41b3e629321dfb
Adham <adam.bolotov@gmail.com>
2022-02-17 17:30:41 +0100

    Initial Commit

---
+++ colourViewUI/colourViewUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  colourViewUIApp.swift
+//  colourViewUI
+//
+//  Created by user213083 on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct colourViewUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
