29b6545937732937b17e4264fa48870de16b3dc6
Adham <adam.bolotov@gmail.com>
2022-02-27 12:24:37 +0100

    end col

--- colourViewUI/ContentView.swift
+++ colourViewUI/ContentView.swift
@@ -3,37 +3,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var redLight: Double = 0.111
+    @State var greenLight: Double = 0.111
+    @State var blueLight: Double = 0.111
+    
+    
     var body: some View {
        
         
-        VStack {
-            Circle()
-                .frame(width: 350.0, height: 350.0)
-                .background(Color.black)
+        VStack(spacing: 75) {
             
-            Button("Button") {
-              changeColour()
+            
+            
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(Color(red: redLight, green: greenLight, blue: blueLight))
+            
+                .frame(minWidth: 200, idealWidth: 355, maxWidth: 365, minHeight: 100, idealHeight: 240, maxHeight: 290)
+            
+            
+            
+           
+            VStack(spacing: 55) {
+                Slider(value: $redLight, in: 0.000...0.999)
+                    .frame(minWidth:150, idealWidth:350, maxWidth: 355)
+                Slider(value: $greenLight, in: 0.000...0.999)
+                    .frame(minWidth:150, idealWidth:350, maxWidth: 355)
+                Slider(value: $blueLight, in: 0.000...0.999)
+                    .frame(minWidth:150, idealWidth:350, maxWidth: 355)
             }
-            .padding(.top, 200.0)
-            .frame(width: 200.0, height: 100.0)
+           
+                
             
-
+            
         }
-        
         
        
     }
 }
 
-
 func changeColour() {
+    
     
         
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    
     }
 }
 
