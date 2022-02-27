46d5f11598749e6c3df5f3090dc9623656e6c2c2
Adham <adam.bolotov@gmail.com>
2022-02-27 12:44:25 +0100

    f

--- colourViewUI/ContentView.swift
+++ colourViewUI/ContentView.swift
@@ -11,32 +11,36 @@ struct ContentView: View {
     
     var body: some View {
        
-        
-        VStack(spacing: 75) {
+      
             
             
-            
-            RoundedRectangle(cornerRadius: 25)
-                .foregroundColor(Color(red: redLight, green: greenLight, blue: blueLight))
-                .padding(.horizontal, 10)
-                .frame(minWidth: 200, idealWidth: 355, maxWidth: 365, minHeight: 100, idealHeight: 240, maxHeight: 290)
-            
-            
-            
-           
-            VStack(spacing: 55) {
-                Slider(value: $redLight, in: 0.000...0.999)
-                    .frame(minWidth:150, idealWidth:350, maxWidth: 355)
-                Slider(value: $greenLight, in: 0.000...0.999)
-                    .frame(minWidth:150, idealWidth:350, maxWidth: 355)
-                Slider(value: $blueLight, in: 0.000...0.999)
-                    .frame(minWidth:150, idealWidth:350, maxWidth: 355)
-            }
-            .padding(.horizontal, 20)
+            VStack(spacing: 75) {
                 
-            
-            
-        }
+                
+                
+                RoundedRectangle(cornerRadius: 25)
+                    .foregroundColor(Color(red: redLight, green: greenLight, blue: blueLight))
+                    .padding(.horizontal, 10)
+                    .frame(minWidth: 200, idealWidth: 355, maxWidth: 365, minHeight: 100, idealHeight: 240, maxHeight: 290)
+                
+                
+                
+               
+                VStack(spacing: 55) {
+                    Slider(value: $redLight, in: 0.000...0.999)
+                        .frame(minWidth:150, idealWidth:350, maxWidth: 355)
+                    Slider(value: $greenLight, in: 0.000...0.999)
+                        .frame(minWidth:150, idealWidth:350, maxWidth: 355)
+                    Slider(value: $blueLight, in: 0.000...0.999)
+                        .frame(minWidth:150, idealWidth:350, maxWidth: 355)
+                }
+                .padding(.horizontal, 20)
+                    
+                
+                
+            }
+        
+       
         
        
     }
